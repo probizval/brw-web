839e533374902da2acab9f5fd266013d9beed899
ashwinpro <sidpatil@Sids-MacBook-Pro.local>
2020-02-13 21:17:51 -0800

    UploadImage

--- src/main/java/com/brw/service/impl/ImageServiceImpl.java
+++ src/main/java/com/brw/service/impl/ImageServiceImpl.java
@@ -6,36 +6,37 @@ package com.brw.service.impl;
  */
 
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 import javax.xml.bind.DatatypeConverter;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.Upload;
 import com.brw.common.constants.Constants;
 import com.brw.dao.ImageDAO;
 import com.brw.dto.ImageDTO;
 import com.brw.dto.ImagesListDTO;
-import com.brw.dto.SearchAgentDTO;
-import com.brw.dto.SearchAgentsListDTO;
 import com.brw.dto.UploadImageDTO;
 import com.brw.dto.UploadImagesListDTO;
 import com.brw.entities.Image;
-import com.brw.entities.SearchAgent;
-import com.brw.exceptions.ImageException;
 
 @Component
 public class ImageServiceImpl implements com.brw.service.ImageService {
@@ -79,11 +80,15 @@ public class ImageServiceImpl implements com.brw.service.ImageService {
 	        byte[] data = DatatypeConverter.parseBase64Binary(strings[1]);
 	        String path = "/Users/sidpatil/Desktop/BizRealWorth/DATA/test_image." + extension;
 	        File file = new File(path);
+	        
 	        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
 	            outputStream.write(data);
+	            
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
+	        
+	        uploadBase64Image(uploadImageDTO.getImageBase64(), extension, "test_image", "test_path");
 
 			returnImageDTOList.add(uploadImageDTO);
 		}
@@ -91,6 +96,35 @@ public class ImageServiceImpl implements com.brw.service.ImageService {
 		returnImagesListDTO.setUploadImagesList(returnImageDTOList);
 		
 		return returnImagesListDTO;
+	}
+	
+	public void uploadBase64Image(String base64String, String fileType, String fileName, String pathToFile) {
+	    InputStream stream = new ByteArrayInputStream(base64String.getBytes());
+
+	    ObjectMetadata metadata = new ObjectMetadata();
+	    metadata.setContentLength(base64String.getBytes().length);
+	    metadata.setContentType("image/"+fileType);
+
+	    //String bucketName = awsS3Configuration.getBucketName();
+	    
+	    String bucketName = "TestS3Bucket";
+
+	    String key = pathToFile + fileName;
+
+	    try {
+	        PutObjectRequest objectRequest = new PutObjectRequest(bucketName, key, stream, metadata);
+	        objectRequest.setCannedAcl(CannedAccessControlList.PublicRead);
+	        
+	        AmazonS3Client s3Client = (AmazonS3Client)AmazonS3ClientBuilder.defaultClient();
+	        s3Client.putObject(objectRequest);
+	        
+	        String s3Url = s3Client.getResourceUrl(bucketName, "some-path/some-key.jpg");
+	        
+	        System.out.println("S3 url is " + s3Url);
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }      
 	}
 	
 	@Override
