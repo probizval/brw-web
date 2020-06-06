74b17b136033be93031fee932b4acce873fc2236
Anisha Jain <jainanisha90@gmail.com>
2020-06-06 12:09:36 -0700

    Integrated with getUserBusiness API, my listing shows all listings now Edit listing is WIP

--- src/main/resources/static/resources/controller/myListingController.js
+++ src/main/resources/static/resources/controller/myListingController.js
@@ -17,16 +17,38 @@
 		if ($scope.myBizList.length%$scope.itemsPerPage !== 0){
 		    $scope.numberOfPages += 1
 		}
+        $('body').on('click', 'li', function() {
+             $('li.active').removeClass('active');
+             $(this).addClass('active');
+        });
+        $scope.changePage = function(pageNumber) {
+            $scope.currentPage = pageNumber - 1
+        }
+        $scope.disablePrevious = function() {
+            if ($scope.currentPage + 1 <= 1){
+                return true
+            } else {
+                $scope.currentPage -= 1
+                return false
+            }
+        };
 
-      $scope.next = function(pagenumber) {
-    	 propertyService.getPropertyListByUser(Number(pagenumber))
-  		.success(function(res) {
-  			$scope.myList = res.data;
-         })
-         .error(function (error) {
-             $scope.status = 'Unable to load property list: ' + error.message;
-         });
-      }
-    }    
+        $scope.disableNext = function() {
+            if ($scope.currentPage + 1 >= $scope.numberOfPages){
+                return false
+            } else {
+                $scope.currentPage += 1
+                return true
+            }
+        };
+        $scope.setItemsPerPage = function(num) {
+          $scope.itemsPerPage = num;
+          $scope.currentPage = 0; //reset to first page
+          $scope.numberOfPages = Math.floor($scope.myBizList.length/$scope.itemsPerPage);
+          if ($scope.myBizList.length%$scope.itemsPerPage !== 0){
+              $scope.numberOfPages += 1
+          }
+        }
+    }
 
 })();
