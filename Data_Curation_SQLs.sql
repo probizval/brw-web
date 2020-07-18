1bdd166a84bd24910fbecb22073f7bee014fadac
sidbrw <spatil@realworth.co>
2020-07-18 16:32:31 -0700

    Update Data_Curation_SQLs.sql

--- Data_Curation_SQLs.sql
+++ Data_Curation_SQLs.sql
@@ -8,6 +8,12 @@
 -- 7. Enter password if it asks - PWD - HDYnCWRnjn8qxTA81y3iNAikCeCJ
 -- 8. Execute SQLs by ending them with ;
 
+
+SELECT * FROM brwdev.t_brw_user_activity order by create_date desc;
+07/13 - 2622
+07/16 - 2626
+07/18 - 2631
+
 SELECT * FROM brwdev.t_brw_user_activity order by create_date desc;
 
 select * from t_brw_business where biz_id = 17821340;
